d993ed7bcd9989fb175cbe0dc1f8726ff6791ed4
zerom-h <cupdisin@gmail.com>
2023-01-25 15:07:32 +0900

    Initial commit

---
+++ src/test/java/io/oauth2/springauthorizationserver/SpringAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.oauth2.springauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
